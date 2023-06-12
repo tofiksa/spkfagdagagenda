d162d5998c934e5f4bdd03b83e552a5df853db72
Tofik Sahraoui <tofiksa@gmail.com>
2023-06-13 01:41:47 +0200

    siste bildene

--- app/page.tsx
+++ app/page.tsx
@@ -17,14 +17,14 @@ export default function IndexPage() {
   <TableHeader>
     <TableRow>
       <TableHead>Klokke</TableHead>
-      <TableHead>Navn</TableHead>
-      <TableHead colSpan={2} className="text-right">Rom</TableHead>
+      <TableHead>Taler</TableHead>
+      <TableHead colSpan={3} className="text-right">Rom</TableHead>
     </TableRow>
   </TableHeader>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium bg-muted">08:30</TableCell>
-      <TableCell colSpan={3} className="text-center bg-muted">Kaffe og frukt</TableCell>
+      <TableCell colSpan={4} className="text-center bg-muted">Kaffe og frukt</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
@@ -36,14 +36,18 @@ export default function IndexPage() {
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">09:15 - 10:00</TableCell>
-      <TableCell colSpan={2} className="text-center"><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://event.dnd.no/dnd/wp-content/uploads/sites/78/2020/10/SW-foredragsholdere-3.jpg"></img></TableCell>
-      <TableCell colSpan={2} className="text-center">Terje Heen, Refaktorering av kode - exposal</TableCell>
+      <TableCell className="text-left"><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://event.dnd.no/dnd/wp-content/uploads/sites/78/2020/10/SW-foredragsholdere-3.jpg"></img>
+      <figcaption className="mt-2 text-sm text-left">Terje Heen</figcaption>
+      </TableCell>
+      <TableCell className="text-left">Refaktorering av kode</TableCell>
+      <TableCell colSpan={2} className="text-right">Exposal</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">10:00 - 11:00</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://media.licdn.com/dms/image/C5603AQEESDVfT5tcyg/profile-displayphoto-shrink_800_800/0/1552135783900?e=2147483647&v=beta&t=WnXBm0flXOj6ZM8VPZDnUM4YgjJzxizROzdTx3VOAxA"></img></TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://media.licdn.com/dms/image/C5603AQEESDVfT5tcyg/profile-displayphoto-shrink_800_800/0/1552135783900?e=2147483647&v=beta&t=WnXBm0flXOj6ZM8VPZDnUM4YgjJzxizROzdTx3VOAxA"></img>
+      <figcaption className="mt-2 text-sm text-left">Stine Midtlund Holmen</figcaption></TableCell>
       <TableCell>Hva skal til for å bli et høyt presterende team?</TableCell>
       <TableCell colSpan={2} className="text-right">Expoområdet</TableCell>
     </TableRow>
@@ -51,58 +55,67 @@ export default function IndexPage() {
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">10:00 - 11:00</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://pbs.twimg.com/profile_images/1488611250/iterate-square-350x349_400x400.png"></img></TableCell>
-      <TableCell>Morten SlaattenBerg - API First</TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://pbs.twimg.com/profile_images/1488611250/iterate-square-350x349_400x400.png"></img>
+      <figcaption className="mt-2 text-sm text-left">Morten SlaattenBerg</figcaption></TableCell>
+      <TableCell>API First</TableCell>
       <TableCell colSpan={2} className="text-right">Exposal</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium bg-muted">11:00 - 12:00</TableCell>
-      <TableCell colSpan={3} className="text-center bg-muted">Lunsj</TableCell>
+      <TableCell colSpan={4} className="text-center bg-muted">Lunsj</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">12:00 - 12:45</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://media.licdn.com/dms/image/C4D03AQF97zk2yM9Q1A/profile-displayphoto-shrink_800_800/0/1637663516596?e=2147483647&v=beta&t=ZbPRAYCz335MEcSFblA-28tUpkPPJA27glMavTUtUdQ"></img></TableCell>      
-      <TableCell colSpan={2}>Product mindset at Oda - Michael Schillingen</TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="https://media.licdn.com/dms/image/C4D03AQF97zk2yM9Q1A/profile-displayphoto-shrink_800_800/0/1637663516596?e=2147483647&v=beta&t=ZbPRAYCz335MEcSFblA-28tUpkPPJA27glMavTUtUdQ"></img>
+      <figcaption className="mt-2 text-sm text-left">Michael Schillingen</figcaption></TableCell>      
+      <TableCell colSpan={2}>Product mindset at Oda</TableCell>
       <TableCell colSpan={2} className="text-right">Expoområdet</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">12:00 - 12:45</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="audun.jpg"></img></TableCell>      
-      <TableCell colSpan={2}>Testdrevet utvikling - Audun Hvenekilde</TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="audun.jpg"></img>
+      <figcaption className="mt-2 text-sm text-left">Audun Hvenekilde</figcaption></TableCell>      
+      <TableCell colSpan={2}>Testdrevet utvikling </TableCell>
       <TableCell colSpan={2} className="text-right">Exposal</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">13:00 - 13:45</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="dagfinb.jpg"></img></TableCell>      
-      <TableCell colSpan={3} className="text-center">Innebygd personvern - Dagfinn Bergsager (exposal)</TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="dagfinb.jpg"></img>
+      <figcaption className="mt-2 text-sm text-left">Dagfinn Bergsager</figcaption></TableCell>
+      <TableCell colSpan={2}>Innebygd personvern</TableCell>
+      <TableCell colSpan={2} className="text-right">Exposal</TableCell>      
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">14:00 - 14:15</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="alejandro.jpg"></img></TableCell>      
-      <TableCell colSpan={3} className="text-center">Designsystemkultur - Alejandro Ortega(exposal)</TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="alejandro.jpg"></img>
+      <figcaption className="mt-2 text-sm text-left">Alejandro Ortega</figcaption></TableCell>      
+      <TableCell colSpan={2}>Designsystemkultur</TableCell>
+      <TableCell colSpan={2} className="text-right">Exposal</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
       <TableCell className="font-medium">14:15 - 15:00</TableCell>
-      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="eirik.jpg"></img></TableCell>      
-      <TableCell colSpan={3} className="text-center">IT Arkeologi - Eirik Eide (exposal)</TableCell>
+      <TableCell><img width={100} height={100} className="h-auto max-w-xs rounded-full" src="eirik.jpg"></img>
+      <figcaption className="mt-2 text-sm text-left">Eirik Eide</figcaption></TableCell>      
+      <TableCell colSpan={2}>IT Arkeologi</TableCell>
+      <TableCell colSpan={2} className="text-right">Exposal</TableCell>
     </TableRow>
   </TableBody>
   <TableBody>
     <TableRow>
-      <TableCell className="font-medium">15:15 - 16:30</TableCell>
-      <TableCell colSpan={3} className="text-center">Stop motion + avslutning</TableCell>
+      <TableCell className="font-medium bg-muted">15:15 - 16:30</TableCell>
+      <TableCell colSpan={4} className="text-center bg-muted">Gruppeaktivitet i expoområdet</TableCell>
     </TableRow>
   </TableBody>
 </Table>
