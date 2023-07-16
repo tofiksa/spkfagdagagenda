61324f7f9c3b8da4f601125bb9f330b30aa0d5ac
Tofik Sahraoui <tofiksa@gmail.com>
2023-07-16 14:21:38 +0200

    lagt til mulighet for å hente profilbilde og endret litt på plasseringen, oppdaterte miljoevariabel i azure_tenant til common for å muliggjøre at flere azure tenants kan logge seg på

--- app/admin/components/login-button.tsx
+++ app/admin/components/login-button.tsx
@@ -1,13 +1,31 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export const LoginButton = () => {
   const { data: session } = useSession();
+  console.log(session);
+  const profileimage = session?.user?.image as string;
   if (session) {
     return (
       <>
-        Signed in as {session?.user?.email} <br />
+        
+        <div className="w-1/4 text-center font-bold">
+                    <div className="w-1/2">
+                        <Image 
+                            alt="profilbilde" 
+                            src={profileimage}
+                            className="h-auto w-auto rounded-full" 
+                            quality={100}
+                            objectFit="contain"
+                            layout="responsive"
+                            width={150}
+                            height={150}
+                            />
+                        <figcaption className="mt-2 text-center text-xs">{session?.user?.name} </figcaption>
+                    </div>
+                </div>
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { SpeakersListContainer } from "@/components/speakers-list-container";
 
 export default function IndexPage() {
   
-
   return (
     <div className="container">
       <SpeakersListContainer /> 
