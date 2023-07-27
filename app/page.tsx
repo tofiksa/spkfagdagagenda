ccaf165bfe52f02e692f54f3090f29eaa9bd78b3
Tofik Sahraoui <tofiksa@gmail.com>
2023-07-28 00:51:27 +0200

    fjernet config i dockerfile

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { SpeakersListContainer } from "@/components/speakers-list-container";
+import { Heading1 } from "lucide-react";
 
 export default function IndexPage() {
   
   return (
-    <div className="container">
-      <SpeakersListContainer /> 
+    <div className="container m-5 bg-slate-300">
+      <div className="header">
+        <h1>Statens pensjonskasse fagdagmal</h1>
+      </div>
     </div>
   )
 }
