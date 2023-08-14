2fc8e9426834818dda8ec7c554fcb79c5f3a7298
Tofik Sahraoui <tofiksa@gmail.com>
2023-08-14 21:58:12 +0200

    redirect til dashboard etter pålogging

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function IndexPage() {
           Her kan du opprette en oversikt over talere og temaer de skal holde
         </p>
         <a href="#" onClick={() => {
-              signIn();
+              signIn(undefined, {callbackUrl: "/dashboard"});
             }} className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
             Logg inn her
             <svg className="ml-2 h-3.5 w-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
